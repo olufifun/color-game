ebd5d3252cab26083413fb591c09a42d54342d55
olufifun <olufifun@gmail.com>
2019-07-18 00:35:46 +0100

    Basic Game flow established.

--- colorGame.js
+++ colorGame.js
@@ -1,25 +1,33 @@
 var colors = [
-    "RGB(255, 0, 0)",
-    "RGB(255, 255, 0)",
-    "RGB(0, 255, 0)",
-    "RGB(0, 255, 255)",
-    "RGB(0, 0, 255)",
-    "RGB(255, 0, 255)",
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
 ]
-
+// Math.floor(Math.random() * 6)
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[Math.floor(Math.random() * 6)];
+var pickedColor = colors[5];
 var colorDisplay = document.getElementById("colorDisplay");
 
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
     //add initial colors to squares
-    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.background = colors[i];
 
     //add click listeners to squares
     squares[i].addEventListener("click", function() {
-    // grab color of clicked square
-    var clickedColor = this.style.backgroundColor;
+
+        // grab color of clicked square
+        var clickedColor = this.style.background;
+
+        //compare clickedColor to pickedColor
+        if (clickedColor === pickedColor){
+            alert ("Correct!");
+        } else {
+            alert("WRONG!!!")
+        }
     });
 }
