e1da40c0a0afbb9a7688b23f0f63965a91873943
olufifun <olufifun@gmail.com>
2019-07-17 23:46:44 +0100

    Set color code to display on h1.

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,10 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[Math.floor(Math.random() * 10)]
+var pickedColor = colors[Math.floor(Math.random() * 6)];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
