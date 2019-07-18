07410d6b2f25110fd079435e66aa6e4a828decca
Olufifun <olufifun@gmail.com>
2019-07-18 13:52:05 +0100

    Added click event listener to New Colors button.

--- colorGame.js
+++ colorGame.js
@@ -5,6 +5,12 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+
+resetButton.addEventListener("click", function(){
+    
+})
 
 colorDisplay.textContent = pickedColor;
 
